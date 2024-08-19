ccb155297d7e6c5579b59920c0d3b668be04b69d
Ricardo Monteiro <rmeasy@gmail.com>
2024-08-19 00:04:28 -0300

    Implementado o Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando Iphone");
+Smartphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
